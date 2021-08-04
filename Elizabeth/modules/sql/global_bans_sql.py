84ad7164620ee0c40aa21a34c7f5cae1febde717
sabkiacademy <81347537+sabkiacademy@users.noreply.github.com>
2021-08-04 17:37:46 +0530

    Update global_bans_sql.py

--- Elizabeth/modules/sql/global_bans_sql.py
+++ Elizabeth/modules/sql/global_bans_sql.py
@@ -1,6 +1,6 @@
 import threading
 
-from elizabeth.modules.sql import BASE, SESSION
+from Elizabeth.modules.sql import BASE, SESSION
 from sqlalchemy import Boolean, Column, Integer, String, UnicodeText
 
 
